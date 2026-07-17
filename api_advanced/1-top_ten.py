3887c41e1a6626bb05d155726f37c568c303dbff
701e423bb52c <dishimwe@gmu.edu>
2026-07-17 04:53:37 -0700

    reddit-api queries

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,17 +5,25 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts, or None if invalid."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "python3:alu.api.advanced:v1.0.0 (by /u/blewis-bump)"
     }
     params = {"limit": 10}
-    response = requests.get(
+
+    try:
+        response = requests.get(
         url,
         headers=headers,
         params=params,
         allow_redirects=False
+        timeout=10
     )
+
+    except:  requests.RequestException:
+        print(None)
+        return
+
     if response.status_code != 200:
         print(None)
         return
